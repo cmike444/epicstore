dd5e09cf14d381e7dc57f0fefebe2d1136df3d8c
Chris Mikelson <chris.mikelson@640labs.com>
2014-02-03 23:28:01 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epicstore::Application.config.secret_key_base = 'bf74d05e3a969c0518c8c00350205bfe43d5f8cefa4c30693cb7218a791dd2d094fe0669a2dc10da59e629f942663763201ce13d5139736187181a325ba356a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Epicstore::Application.config.session_store :cookie_store, key: '_epicstore_session'
