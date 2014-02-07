9f4e93c05851d6e64239f44ca393b6432c95df25
Chris Mikelson <chris.mikelson@640labs.com>
2014-02-07 01:33:24 -0600

    Update Amazon s3 for Paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Epicstore::Application.configure do
 
   # Required for Heroku
   config.action_mailer.default_url_options = { :host => 'epicsteme.heroku.com' }
+
+  # Required for Amazon S3 Web Storage
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
